a0721e23e032d46032586193f5c128f9931690f2
alexandre b defreitas cs student <abd9@afsconnect2.njit.edu>
2014-03-24 18:22:23 -0400

    changed example code to implement a getCurrentTime method to see if the time of both instances of the singleton give me the same now-time.

--- js/singleton.js
+++ js/singleton.js
@@ -3,20 +3,28 @@ var mySingleton = (function () {
 	var instance;
 	
 	function init() {
-		function privateMethod() {
-			console.log( "I am private" );
-		}
 		
-		var privateVariable = "I'm also private";
-		var privateRandomNumber = Math.random();
+		
+		//function privateMethod() {
+		//	console.log( "I am private" );
+		//}
+		
+		//var privateVariable = "I'm also private";
+		//var privateRandomNumber = Math.random();
 		
 		return {
-			publicMethod: function() {
-				console.log( "The public can see me!" );
-			},
-			publicProperty: "I am also public",
-			getRandomNumber: function() {
-				return privateRandomNumber;
+			//publicMethod: function() {
+			//	console.log( "The public can see me!" );
+			//},
+			//publicProperty: "I am also public",
+			getCurrentTime: function() {
+				var myDate = new Date();
+				var hours = myDate.getHours();
+				var minutes = myDate.getMinutes();
+				var seconds = myDate.getSeconds();
+				var milliseconds = myDate.getMilliseconds();
+				var now = "Time: " + hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
+				return now;
 			}
 		};
 	};
@@ -33,5 +41,12 @@ var mySingleton = (function () {
 })();
 
 var singleA = mySingleton.getInstance();
-var singleB = mySingleton.getInstance();
-console.log( singleA.getRandomNumber() === singleB.getRandomNumber());
+var timeA = singleA.getCurrentTime();
+setTimeout(function() {
+	var singleB = mySingleton.getInstance();
+	var timeB = singleB.getCurrentTime();
+}, 3000);
+
+console.log("timeA = " + timeA);
+console.log("timeB = " + timeB);
+console.log( singleA.getCurrentTime() === singleB.getCurrentTime());
