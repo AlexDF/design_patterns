0eb68a9817970583e8bae60e451da1380b10a3aa
alexandre b defreitas cs student <abd9@afsconnect2.njit.edu>
2014-03-25 12:48:12 -0400

    Observer skeleton added

--- js/observer.js
+++ js/observer.js
@@ -58,3 +58,15 @@ Subject.prototype.notify = function( context ) {
 	}
 };
 
+// We then define a skeleton for creating new Observers.
+// The 'update' functionality here will be overwritten later
+// with custom behavior.
+
+//The Observer
+function Observer() {
+	this.update = function() {
+		// ...
+	};
+}
+
+
