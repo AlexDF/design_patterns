210bbd8c95492fe5c3a5fdce214afb37152d4304
alexandre b defreitas cs student <abd9@afsconnect2.njit.edu>
2014-03-25 12:40:55 -0400

    implemented Subject model from example

--- js/observer.js
+++ js/observer.js
@@ -1,3 +1,5 @@
+//First, let's model the list of dependent Observers
+//a subject may have:
 function ObserverList() {
 	this.observerList = [];
 }
@@ -34,9 +36,25 @@ ObserverList.prototype.removeAt = function( index ) {
 };
 
 
+//Next, let's model the Subject and the ability to add,
+//remove, or notify observers on the observer list.
 
+function Subject() {
+	this.observers = new ObserverList();
+}
 
+Subject.prototype.addObserver = function( observer ) {
+	this.observers.add( observer );
+};
 
+Subject.prototype.removeObserver = function( observer ) {
+	this.observers.removeAt( this.observers.indexOf( observer, 0 ) );
+};
 
-
+Subject.prototype.notify = function( context ) {
+	var observerCount = this.observers.count();
+	for( var i = 0; i < observerCount; i++ ) {
+		this.observers.get(i).update( context );
+	}
+};
 
