043722f90e643272adebbf98933710ac510ceb3a
alexandre b defreitas cs student <abd9@afsconnect2.njit.edu>
2014-03-28 21:52:57 -0400

    command pattern book example - part 2 completed. Ready to make my own.

--- js/command.js
+++ js/command.js
@@ -16,4 +16,27 @@
 			return "You have successfully booked a viewing of " + model + " ( " + id + " ).";
 		}
 	};
+	
+	CarManager.execute = function ( name ) {
+		return CarManager[name] && CarManager[name].apply( CarManager, [].slice.call(arguments, 1) );
+	};
+	
+	console.log( CarManager.execute( "arrangeViewing", "Ferrari", "14523" ) );
+	console.log( CarManager.execute( "requestInfo", "Ford Mondeo", "54323" ) );
+	console.log( CarManager.execute( "requestInfo", "Ford Escort", "34232" ) );
+	console.log( CarManager.execute( "buyVehicle", "Ford Escort", "34232" ) );
+	
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
