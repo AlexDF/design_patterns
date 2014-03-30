86a5acb32ac4b05421a3b2b6119db51b47a88d5a
alexandre b defreitas cs student <abd9@afsconnect2.njit.edu>
2014-03-30 18:48:50 -0400

    minor changes made

--- js/facade.js
+++ js/facade.js
@@ -3,10 +3,10 @@ function getEmployeeList() {
 	$.getJSON("json/employees.json", function(employee) {
 		$.each(employee, function() {
 			$.each(this, function(key, value) {
-				$("#employee_list").append(
+				$("#facade_output").append("<p>" +
 					"First: " + value.firstname + "<br>" +
 					"Last: " + value.lastname + "<br>" +
-					"Phone: " + value.phone + "<br><br>"
+					"Phone: " + value.phone + "</p>"
 				);
 			});
 		});
