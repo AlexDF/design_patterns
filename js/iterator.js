5fffd903f4fcd56a30669aa04d1d610c367e3e88
alexandre b defreitas cs student <abd9@afsconnect2.njit.edu>
2014-04-02 19:02:56 -0400

    tests for iterator pattern completed

--- js/iterator.js
+++ js/iterator.js
@@ -1,12 +1,3 @@
-/*$.each( ["john", "dave", "rick", "julian"], function( index, value ) {
-	console.log( index + ": " + value);
-});
-
-$( "li" ).each( function ( index ) {
-	console.log( index + ": " + $( this ).text() );
-});*/
-
-
 var names = ["Jake", "ADAM", "Sarah", "LINDA", "Peter"];
 
 var myObj = {
